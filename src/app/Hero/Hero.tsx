808d8b504e175cff2bfab22e9ad3c508d2046bfe
KHP53 <khp4121@gmail.com>
2024-06-18 22:29:47 -0230

    completed footer with github, linkedin and website links

--- src/app/Hero/Hero.tsx
+++ src/app/Hero/Hero.tsx
@@ -2,17 +2,20 @@ import React from 'react'
 
 export default function Hero() {
     return (
-        <div className="flex items-center justify-center h-screen">
-            <div className="flex flex-col items-center">
-                <h1 className="text-4xl text-white font-bold mainText">Welcome to Start Up Research AI</h1>
-                <h3 className="text-1xl text-white font-normal">Customizable, helps you to do a deep research before getting started</h3>
-                <h3 className="text-1xl text-white font-normal">on the start up eco-system!</h3>
+        <div className="flex items-center justify-between h-screen px-10">
+            <div className="flex flex-col items-start">
+                <h1 className="text-4xl text-white font-bold text-left mainText">Welcome to Start Up Research AI</h1>
+                <h3 className="text-1xl text-white font-normal text-left">Customizable, helps you to do a deep research before getting started</h3>
+                <h3 className="text-1xl text-white font-normal text-left">on the start up eco-system!</h3>
                 <button className="px-4 py-2 text-blue-500 border border-blue-500 rounded-full inline-flex items-center bg-transparent getStartedBtn">
                     Get Started
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#3B82F6">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4l8 8-8 8" />
                     </svg>
                 </button>
+            </div>
+            <div className='heroImg'>
+                <img src="https://cdn.dribbble.com/userupload/5276458/file/original-5cb192ab80511f018f5253d7bbe123da.png?resize=752x682&vertical=center" className="max-w-xl rounded-lg shadow-2xl" />
             </div>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./Navbar/Navbar";
 import Hero from "./Hero/Hero";
+import Footer from "./Footer/footer";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <Footer />
     </main >
   );
 }
