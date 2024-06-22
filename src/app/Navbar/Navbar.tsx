22b2e9c5eab8d98ee06760cffbe74ad6e65738cf
KHP53 <khp4121@gmail.com>
2024-06-22 12:06:08 -0230

    added home links to proper buttons

--- src/app/Navbar/Navbar.tsx
+++ src/app/Navbar/Navbar.tsx
@@ -9,16 +9,16 @@ export default function Navbar() {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="white"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a className="text-white">Home</a></li>
+                            <li><a href="/" className="text-white">Home</a></li>
                             <li><a className="text-white">About</a></li>
                             <li><a className="text-white">Contact Us</a></li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl text-white">SUR•AI</a>
+                    <a href="/" className="btn btn-ghost text-xl text-white">SUR•AI</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        <li><a className="text-white">Home</a></li>
+                        <li><a href="/" className="text-white">Home</a></li>
                         <li><a className="text-white">About</a></li>
                         <li><a className="text-white">Contact Us</a></li>
                     </ul>
